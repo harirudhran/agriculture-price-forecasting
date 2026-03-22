85c56215ac1d5af3a83e73445e6405aa73dfcf21
Hari rudhran <mharirudhran@gmail.com>
2026-03-22 10:27:38 +0530

    Fixed ALLOWED_HOSTS and disabled DEBUG

--- agri_forecast/settings.py
+++ agri_forecast/settings.py
@@ -120,9 +120,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+import os
 STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
